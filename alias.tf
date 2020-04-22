c44bb5f27f5da3442f5ff6e71961b86d0a5f706d
etwillbefine <tim.gatzemeier@gmx.de>
2020-04-22 13:05:20 +0200

    remove hosted zone name from fqdn for record name

--- alias.tf
+++ alias.tf
@@ -1,11 +1,10 @@
-
 module "records" {
   source        = "git::https://github.com/goci-io/aws-route53-records.git?ref=tags/0.3.0"
   enabled       = var.create_alias_record
   hosted_zone   = var.hosted_zone_name
   alias_records = [
     {
-      name       = var.alias_record_fqdn
+      name       = replace(var.alias_record_fqdn, var.hosted_zone_name, "")
       alias      = join("", aws_s3_bucket.redirect.*.website_domain)
       alias_zone = join("", aws_s3_bucket.redirect.*.hosted_zone_id)
     }
